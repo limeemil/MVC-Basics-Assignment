c01ae1c12f46fcc3649564212439ab0fff4f31f2
Andreas Lönnermark <andreas.lonnermark@gmail.com>
2022-01-03 17:55:17 +0100

    Added React components

--- Controllers/ReactController.cs
+++ Controllers/ReactController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MVC_Basics__Assignment.Context;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +9,22 @@ namespace MVC_Basics__Assignment.Controllers
 {
     public class ReactController : Controller
     {
+        private readonly PeopleDatabaseContext _context;
+
+        public ReactController(PeopleDatabaseContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        public JsonResult GetPersonData()
+        {
+            var data = _context.People.OrderBy(a => a.FirstName).ToList();
+            return new JsonResult ( data, new Newtonsoft.Json.JsonSerializerSettings());
         }
     }
 }
--- wwwroot/js/ReactScript.jsx
+++ wwwroot/js/ReactScript.jsx
@@ -1,4 +1,4 @@
-﻿class PersonBox extends React.Component {
+﻿/*class PersonBox extends React.Component {
     render() {
         return (
             <div className="personBox">Hello, world! I am a person box! </div>
@@ -6,4 +6,58 @@
     }
 }
 
-ReactDOM.render(<PersonBox />, document.getElementById('content'));
+ReactDOM.render(<PersonBox />, document.getElementById('content'));*/
+
+/*var PersonGridRow = React.createClass({
+    render: function () {
+        return (
+            <tr>
+                <td>{this.props.item.FirstName}</td>
+                <td>{this.props.item.LastName}</td>
+                <td>{this.props.item.Phonenumber}</td>
+                <td>{this.props.item.City}</td>
+            </tr>
+        );
+    }
+});
+
+var PersonGridTable = React.createClass({
+    getInitialState: function () {
+        return {
+            items: []
+        }
+    },
+    componentDidMount: function () {
+        @* Fetch data via ajax *@
+        $.get(this.props.dataUrl, function (data) {
+            if (this.isMounted()) {
+                this.setState({
+                    items: data
+                });
+            }
+        }.bind(this));
+    },
+    render: function () {
+        var rows = [];
+        this.state.items.forEach(function (item) {
+            rows.push(<PersonGridRow key={item.SSN} item={item} />);
+        });
+        return (<table className="table table-bordered table-responsive">
+            <thead>
+                <tr>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Phonenumber</th>
+                    <th>City</th>
+                </tr>
+            </thead>
+            <tbody>
+                {rows}
+            </tbody>
+        </table>);
+    }
+});
+ReactDOM.render(
+    <PersonGridTable dataUrl="/home/getPersonData" />,
+    document.getElementById('griddata')
+);*/
